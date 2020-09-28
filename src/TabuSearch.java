4465a90a8921ddad5c5ba079508235becfb3574a
Felix Caron <felix.caron@umontreal.ca>
2020-09-28 11:36:29 -0400

    Only consider one tabu solution at each iteration

--- src/TabuSearch.java
+++ src/TabuSearch.java
@@ -16,59 +16,60 @@ public class TabuSearch {
     }
 
     public Solution Apply(Solution startingSolution, List<Request> requests) {
-        Solution currentSolution;
-        Solution nextSolution = startingSolution;
+        Solution currentSolution = startingSolution;
         Solution bestSolution = startingSolution;
 
         int[] tabuCounters = new int[requests.size()];
         int noImprovementCounter = 0;
 
         do {
-            currentSolution = nextSolution;
-            nextSolution = null;
-            int requestIndex = -1;
+            Solution bestTemporarySolution = null;
+            Solution bestTabuTemporarySolution = null;
+            int bestRequestIndex = -1;
+            int bestTabuRequestIndex = -1;
 
             ++noImprovementCounter;
 
             for (int i = 0; i < requests.size(); ++i) {
+                Solution temporarySolution = new Solution(currentSolution);
+                temporarySolution.removeRequest(requests.get(i));
+
                 if (tabuCounters[i] == 0) {
-                    Solution temporarySolution = new Solution(currentSolution);
-                    temporarySolution.removeRequest(requests.get(i));
-                    if (nextSolution == null ||
-                            DoubleComparator.lessThan(temporarySolution.getCost(), nextSolution.getCost())) {
-                        nextSolution = temporarySolution;
-                        requestIndex = i;
+                    if (bestTemporarySolution == null ||
+                            DoubleComparator.lessThan(temporarySolution.getCost(), bestTemporarySolution.getCost())) {
+                        bestTemporarySolution = temporarySolution;
+                        bestRequestIndex = i;
                     }
-                }
-            }
-
-            assert nextSolution != null;
-            double costBeforeReinsertion = nextSolution.getCost();
-            nextSolution.insertRequest(requests.get(requestIndex));
-            if (DoubleComparator.lessThan(nextSolution.getCost(), bestSolution.getCost())) {
-                bestSolution = nextSolution;
-                noImprovementCounter = 0;
-            }
-
-            for (int i = 0; i < requests.size(); ++i) {
-                if (tabuCounters[i] > 0) {
-                    Solution temporarySolution = new Solution(currentSolution);
-                    temporarySolution.removeRequest(requests.get(i));
-                    if (DoubleComparator.lessThan(temporarySolution.getCost(), costBeforeReinsertion)) {
-                        temporarySolution.insertRequest(requests.get(i));
-                        if (DoubleComparator.lessThan(temporarySolution.getCost(), bestSolution.getCost())) {
-                            nextSolution = temporarySolution;
-                            requestIndex = i;
-                            bestSolution = temporarySolution;
-                            noImprovementCounter = 0;
-                        }
+                } else {
+                    if (bestTabuTemporarySolution == null || DoubleComparator.lessThan(temporarySolution.getCost(),
+                            bestTabuTemporarySolution.getCost())) {
+                        bestTabuTemporarySolution = temporarySolution;
+                        bestTabuRequestIndex = i;
                     }
 
                     --tabuCounters[i];
                 }
             }
 
-            tabuCounters[requestIndex] = tabuTenure;
+            assert bestTemporarySolution != null;
+            bestTemporarySolution.insertRequest(requests.get(bestRequestIndex));
+            currentSolution = bestTemporarySolution;
+            if (DoubleComparator.lessThan(bestTemporarySolution.getCost(), bestTemporarySolution.getCost())) {
+                bestSolution = bestTemporarySolution;
+                noImprovementCounter = 0;
+            }
+
+            if (bestTabuTemporarySolution != null) {
+                bestTabuTemporarySolution.insertRequest(requests.get(bestTabuRequestIndex));
+                if (DoubleComparator.lessThan(bestTabuTemporarySolution.getCost(), bestSolution.getCost())) {
+                    currentSolution = bestTabuTemporarySolution;
+                    bestSolution = bestTabuTemporarySolution;
+                    bestRequestIndex = bestTabuRequestIndex;
+                    noImprovementCounter = 0;
+                }
+            }
+
+            tabuCounters[bestRequestIndex] = tabuTenure;
 
             ++totalNbIterations;
         } while (noImprovementCounter < noImprovementStoppingCriteria);
